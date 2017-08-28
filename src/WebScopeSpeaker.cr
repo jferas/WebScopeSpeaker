ed773c202d5474bfd3dcfef1f289fb54bac697e5
John Feras <jferas@ferasinfotech.com>
2017-08-28 08:07:32 -0400

    got the request for user sent to periscope

--- src/WebScopeSpeaker.cr
+++ src/WebScopeSpeaker.cr
@@ -1,10 +1,26 @@
 require "kemal"
+require "crustache"
 require "./WebScopeSpeaker/*"
 
 module WebScopeSpeaker
 
+# Serve the web page
 get "/" do
   render "views/index.ecr"
+end
+
+# Serve the response to get live chat room info about a user
+get "/:user" do |env|
+  user = env.params.url["user"]
+  puts "we have a user of #{user}"
+  response = HTTP::Client.get("https://www.periscope.tv/" + user)
+  if response.status_code == 200
+    puts "We got a response of #{response.body}"
+  else
+    puts "We got an error of #{response.status_code}"
+  end
+
+  "Saw your request for data about #{user}"
 end
 
 Kemal.run
